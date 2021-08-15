a720b292baaf7486bff5922d3dc8dbf9a78856c9
Gabriel Cassanta <cassantagabriel@gmail.com>
2021-08-15 14:21:34 -0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
-import { Button } from './components/Button'
+// import { Button } from './components/Button'
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
-    <>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </>
+    <NewRoom />
   );
 }
 
